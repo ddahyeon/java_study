b191b332018c6a82a4cb99c6054184b089ef42ac
ddahyeon <dhtjtj0130@naver.com>
2026-04-09 11:39:44 +0900

    java

--- HelloTest/src/exam17_Utility/EunmTest.java
+++ HelloTest/src/exam17_Utility/EunmTest.java
@@ -1,5 +1,7 @@
 package exam17_Utility;
 
+import java.util.Arrays;
+
 public class EunmTest {
 
 	public static void main(String[] args) {
@@ -12,6 +14,11 @@ public class EunmTest {
 		
 		default:System.out.println("dafault");break;
 		}
+		//Day의 enum의 메서드  
+		System.out.println("name: "+day.name());  // 상수값을 나타냄 
+		System.out.println("ordinal: "+day.ordinal());  // 위치를 나타냄  
+		Day[] days=day.values(); // 상수값들을 배열로 넣기  
+		System.out.println(Arrays.toString(days)); // 배열 출력  arrays.toString  
 
 	}
 
