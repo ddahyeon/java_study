31aac0db3dacf40fce55146d79597cdcdb03d99d
ddahyeon <dhtjtj0130@naver.com>
2026-04-13 14:14:57 +0900

    java

--- HelloTest/src/exam22_StreamAPI/StreamTest03_중간처리.java
+++ HelloTest/src/exam22_StreamAPI/StreamTest03_중간처리.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamTest03_중간처리 {
@@ -29,7 +31,7 @@ public class StreamTest03_중간처리 {
 		stream.forEach(System.out::println);
 		System.out.println("============================");
 		
-		//중간처리 - 중복제거 distinct()
+		//중간처리1 - 중복제거 distinct()
 //		stream = list.stream();
 //		Stream<String> stream2 = stream.distinct();
 //		stream2.forEach(System.out::println);
@@ -37,6 +39,94 @@ public class StreamTest03_중간처리 {
 		list.stream()
 		    .distinct()
 		    .forEach(System.out::println);
+		System.out.println("============================");
 		
+		//중간처리2 - 필터링 - filter(Predicate)
+		stream = list.stream();
+		
+		//익명클래스
+		Predicate<String> p = new Predicate<String>() {
+			@Override
+			public boolean test(String s) {
+				return s.length() == 3;
+			}
+		};
+		//람다표현식
+		Predicate<String> p2 = s -> s.length() == 3;
+		Stream<String> stream2 = stream.filter(p2);
+		stream2.forEach(System.out::println);
+		
+		//최종 코드
+		list.stream()
+		    .filter(s -> s.length() == 3)
+		    .forEach(System.out::println);
+		
+		System.out.println("============================");
+		// distinct() + filter(()
+		//최종 코드
+		list.stream()
+		    .distinct()
+		    .filter(s -> s.length() == 3)
+		    .forEach(System.out::println);
+		
+		
+		System.out.println("============================");
+		//중간처리3 - 가공 map(Function<T,R> )
+		List<Student> list2 = Arrays.asList(new Student("홍길동", 100),
+											new Student("이순신", 90),
+											new Student("유관순", 50),
+											new Student("강감찬", 70),
+											new Student("정조", 95),
+											new Student("선덕여왕", 45)
+				                            );
+		// 이름만 출력
+		Stream<Student> stream4 = list2.stream();
+		//익명함수
+		Function<Student, String> f = new Function<Student, String>() {
+			@Override
+			public String apply(Student stu) {
+				return stu.getName();
+			}
+		};
+		//람다표현식
+		Function<Student, String> f2 = stu->stu.getName();
+		//메서드참조
+		Function<Student, String> f3 = Student::getName;
+		Stream<String> stream5 = stream4.map(f3);
+		stream5.forEach(System.out::println);
+		System.out.println("============================");
+		
+		//최종코드
+		list2.stream()
+		     .map(Student::getName)
+		     .forEach(System.out::println);
+		
+		System.out.println("============================");
+	}
+}
+
+class Student{
+	String name; //이름
+	int grade;   //점수
+	public Student() {}
+	public Student(String name, int grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", grade=" + grade + "]";
 	}
 }
